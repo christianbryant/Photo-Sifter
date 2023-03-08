cc2b841d2bab66443137f0494b81a5effcccd78f
TheRetroGuy2020 <sandslinger14@gmail.com>
2023-03-07 16:35:17 -0800

    Updated Errors

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ def import_list(file_list, raw_type):
     global files_list
     count = 0
     Lines = file_list.readlines()
-    if Lines == 0:
-        print("No lines in provide file!")
-        return
     for line in Lines:
         temp = line.strip() + raw_type
         count += 1
         files_list.append(temp)
+    if count == 0:
+        raise Exception("No lines in provided file!")
     print("Found %s file names in file provided" % str(count))
     
 def sort_folder(name_of_destination):
@@ -32,6 +31,8 @@ def sort_folder(name_of_destination):
         print("Folder moved to %s" % str(destination))
     os.chdir(source)
     files = os.listdir()
+    if len(files) <= 3:
+        raise Exception("No image files present!")
     for i in files:
         curr_file = os.path.join(os.getcwd(),i)
         file_name = os.path.basename(curr_file)
@@ -42,19 +43,19 @@ def sort_folder(name_of_destination):
         if count >= len(files_list):
             print("Moved %s files to folder %s" % (str(count), str(destination)))
             return
-
+    if count == 0:
+        raise Exception("Found none of the files that were provided in the file list!")
 def main():
     if (len(sys.argv) <= 4)&(len(sys.argv) > 1):
         destination = str(sys.argv[1])
         file_list = str(sys.argv[2])
         raw_type = str(sys.argv[3])
     else:
-        print("Please provide command line input as follows:\n python3 main.py DESTINATION_FOLDER_NAME FILE_LIST.txt RAW_TYPE")
-        return
+        raise Exception("Please provide command line input as follows:\n python3 main.py DESTINATION_FOLDER_NAME FILE_LIST.txt RAW_TYPE")
     try:
         file = open(file_list)
     except:
-        print("Could not find the file list")
+        raise Exception("Could not find the file list")
 
     import_list(file, raw_type)
     sort_folder(destination)
